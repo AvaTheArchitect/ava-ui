17c5dd71f7d51cafba8a02be3b182025acf52698
AvaTheArchitect <brettbolzenthal@gmail.com>
2025-11-05 11:59:16 -0800

    Small change in last line of comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -131,4 +131,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}// Force Vercel refresh Mon Oct 27 17:14:44 PDT 2025
+}// Key change: Cursor will be disabled in landscape, let AlphaTab scroll normally
